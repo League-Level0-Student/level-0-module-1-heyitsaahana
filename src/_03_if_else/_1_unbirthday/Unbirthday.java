9cfbe03066a006eb030b1828f5411395bfa93eb9
league <league@192.168.1.87>
2022-11-26 12:58:53 -0800

    VennDiagram,brithday

---
+++ src/_03_if_else/_1_unbirthday/Unbirthday.java
@@ -0,0 +1,28 @@
+package _03_if_else._1_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+	public static void main(String[] args) {
+		String birthday=JOptionPane.showInputDialog("When is your birthday?");
+		if(birthday.equals(11/26)) {
+			JOptionPane.showMessageDialog(null, "Happy Birthday");
+			
+		}
+		if(!birthday.equals(11/26)) { 
+			JOptionPane.showMessageDialog(null, "Happy Unbirthday");
+			
+		}
+		
+		
+		
+	}
+}
+		
+		
+			
+		
+	
+
+
+
